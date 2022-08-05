690cd9ed8f3b687a1b280951eb70f0a4b518e802
taek0622 <min4644@gmail.com>
2022-08-05 18:36:59 +0900

    Feat✨: Add Project

---
+++ IntoHistory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntoHistory
+//
+//  Created by 김민택 on 2022/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
