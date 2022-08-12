28709a0a65c48c0fc15f8ee5b043e9b10ff56b1c
JI SOO <dlwltn815@gmail.com>
2022-08-12 17:50:33 +0900

    Feat✨: 아울렛변수 생성

---
+++ IntoHistory/Screens/ARView/ARViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ARViewController.swift
+//  IntoHistory
+//
+//  Created by LeeJiSoo on 2022/08/12.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ARViewController: UIViewController, ARSCNViewDelegate {
+
+    // MARK: - View
+
+    @IBOutlet var arscene: ARSCNView!
