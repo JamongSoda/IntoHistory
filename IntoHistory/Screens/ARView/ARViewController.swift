37605f3c8740678102d7904ca8b9cca40c5172c3
JI SOO <dlwltn815@gmail.com>
2022-08-12 17:51:21 +0900

    Feat✨: addARView 메서드 생성

--- IntoHistory/Screens/ARView/ARViewController.swift
+++ IntoHistory/Screens/ARView/ARViewController.swift
@@ -14,3 +14,22 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     // MARK: - View
 
     @IBOutlet var arscene: ARSCNView!
+    private func addARView() {
+        self.arscene = ARSCNView(frame : self.view.frame)
+        self.view.addSubview(arscene)
+
+        arscene.delegate = self
+        arscene.showsStatistics = true
+
+        let scene = SCNScene()
+        let sphere = SCNSphere(radius: 0.2)
+        sphere.firstMaterial?.diffuse.contents = UIImage(named : "testImage.jpg")
+
+        let sphereNode = SCNNode(geometry : sphere)
+        sphereNode.position = SCNVector3(0.5, 0.1, -1)
+        scene.rootNode.addChildNode(sphereNode)
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target : self, action : #selector(tapped))
+        self.arscene.addGestureRecognizer(tapGestureRecognizer)
+        arscene.scene = scene
+    }
