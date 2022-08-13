41100f0561daefd7d7d13f22dacd39b75ec72298
taek0622 <min4644@gmail.com>
2022-08-13 19:18:15 +0900

    Design:iphone:: #64 Add herodetailview
    - 호국 영웅 상세 뷰 추가

---
+++ IntoHistory/HeroDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HeroDetailViewController.swift
+//  IntoHistory
+//
+//  Created by 김민택 on 2022/08/13.
+//
+
+import UIKit
+
+class HeroDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
