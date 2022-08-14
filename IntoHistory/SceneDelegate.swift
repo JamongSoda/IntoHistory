ef7553b64a21a79893c19532d5975b98afbb453c
GODNOEL <103012104+GODNOEL@users.noreply.github.com>
2022-08-14 10:30:00 +0900

    Style🎨: #25 Modify Code Style
    SceneDelegate 네비게이션 변수 수정

--- IntoHistory/SceneDelegate.swift
+++ IntoHistory/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let nav = UINavigationController(rootViewController: MainViewController())
-        window?.rootViewController = nav
+        let navi = UINavigationController(rootViewController: MainViewController())
+        window?.rootViewController = navi
         window?.makeKeyAndVisible()
     }
 
