4c75a590122b4fb40a8706f5665da1163b362f3d
taek0622 <min4644@gmail.com>
2022-08-14 19:37:04 +0900

    Feat:sparkles:: #70 Add CoreData Struct
    - 코어데이터 데이터 구조 추가

---
+++ IntoHistory/Coredata/CourseEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  CourseEntity+CoreDataClass.swift
+//  IntoHistory
+//
+//  Created by 김민택 on 2022/08/14.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CourseEntity)
+public class CourseEntity: NSManagedObject {
+
+}
---
+++ IntoHistory/Coredata/PinEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  PinEntity+CoreDataClass.swift
+//  IntoHistory
+//
+//  Created by 김민택 on 2022/08/14.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(PinEntity)
+public class PinEntity: NSManagedObject {
+
+}
