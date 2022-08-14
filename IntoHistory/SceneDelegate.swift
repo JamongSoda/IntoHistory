de09371850af91c7188da30d9a2c429f9ef227a1
taek0622 <min4644@gmail.com>
2022-08-14 15:39:43 +0900

    Chore:wrench:: Delete Overlapped Code
    - 중복되어 문제가 발생할 수 있는 코드 삭제

--- IntoHistory/SceneDelegate.swift
+++ IntoHistory/SceneDelegate.swift
@@ -17,11 +17,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         // guard let _ = (scene as? UIWindowScene) else { return }
-        guard let scene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: scene)
-        let navi = UINavigationController(rootViewController: HeroListViewController())
-        window?.rootViewController = navi
-        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
