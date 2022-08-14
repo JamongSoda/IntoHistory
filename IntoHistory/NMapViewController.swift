9d45ddf3e99d34534d90405ad278993c470561ea
taek0622 <min4644@gmail.com>
2022-08-14 23:21:16 +0900

    !HOTFIX:ambulance:: #75 Restore Lost File
    - 집 나갔던 NMapViewController.swift 파일 복구

---
+++ IntoHistory/NMapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NMapViewController.swift
+//  IntoHistory
+//
+//  Created by 지준용 on 2022/08/11.
+//
+import CoreLocation
+import UIKit
+
+import NMapsMap
+
+class NMapViewController: UIViewController, CLLocationManagerDelegate {
+
+    var locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let naverMapView = NMFNaverMapView(frame: view.frame)
+        view.addSubview(naverMapView)
+
+        naverMapView.showZoomControls = false
+        naverMapView.showLocationButton = true
+
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+}
