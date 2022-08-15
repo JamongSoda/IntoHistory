0a8e76b8c4141e62adc62ec86cdb2ae701793734
Juny0110 <wnsdyds403@naver.com>
2022-08-15 16:25:04 +0900

    Feat:sparkles:: #45 Load Pin Location of JSON Data
    - JSON데이터의 x,y 좌표 값에 따른 지도에서 핀 위치 호출

--- IntoHistory/NMapViewController.swift
+++ IntoHistory/NMapViewController.swift
@@ -11,23 +11,43 @@ import NMapsMap
 	
 class NMapViewController: UIViewController, CLLocationManagerDelegate {
 
+    private let courseJSONLoader = LoadingCourseJSON().courses
     var locationManager = CLLocationManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let naverMapView = NMFNaverMapView(frame: view.frame)
+
         view.addSubview(naverMapView)
-
-        naverMapView.showZoomControls = false
-        naverMapView.showLocationButton = true
-
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
+        navigationItem.title = "상세 코스"
         
         if CLLocationManager.locationServicesEnabled() {
             locationManager.startUpdatingLocation()
         }
+
+        // 지정 좌표로 시작 화면 띄우기
+        let cameraUpdate = NMFCameraUpdate (
+            scrollTo: NMGLatLng(
+                lat: courseJSONLoader[1].course_pins[0].pin_x!,
+                lng: courseJSONLoader[1].course_pins[0].pin_y!
+            )
+        )
+        cameraUpdate.animation = .easeIn
+        naverMapView.mapView.moveCamera(cameraUpdate)
+        
+        for pinNum in 0 ..< courseJSONLoader[1].course_pins.count {
+            let marker = NMFMarker()
+
+            marker.iconImage = NMFOverlayImage(
+                image: UIImage(imageLiteralResourceName: SelectedTypes(rawValue: pinNum + 1)?
+                        .selectedPinsImage(isSelecting: false) ?? ""))
+            marker.position = NMGLatLng(
+                lat: courseJSONLoader[1].course_pins[pinNum].pin_x ?? 0,
+                lng: courseJSONLoader[1].course_pins[pinNum].pin_y ?? 0
+            )
+            marker.mapView = naverMapView.mapView
+        }
     }
 }
+
