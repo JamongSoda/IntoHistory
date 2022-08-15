b1ee5f23ed95ac675e381e72db7db636a6016eb7
taek0622 <min4644@gmail.com>
2022-08-16 01:55:41 +0900

    Feat:sparkles:: #98 Add Modal Close Function
    - 모달에서 닫기 버튼을 클릭했을 때 모달을 닫는 기능 추가

--- IntoHistory/HeroDetailViewController.swift
+++ IntoHistory/HeroDetailViewController.swift
@@ -56,6 +56,7 @@ class HeroDetailViewController: UIViewController {
 
         attribute()
         layout()
+        setButtonGesture()
     }
     
     // MARK: - Method
@@ -114,6 +115,16 @@ class HeroDetailViewController: UIViewController {
             paddingBottom: 60,
             paddingRight: 20
         )
+    }
+    
+    private func setButtonGesture() {
+        let tapCloseButton = UITapGestureRecognizer(target: self, action: #selector(self.tapCloseButton(_:)))
+        closeButton.addGestureRecognizer(tapCloseButton)
+        closeButton.isUserInteractionEnabled = true
+    }
+    
+    @objc func tapCloseButton(_ sender: UITapGestureRecognizer) {
+        self.dismiss(animated: false, completion: nil)
     }
 }
 
