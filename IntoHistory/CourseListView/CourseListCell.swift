a57b5520e9ac8376babf6323ce35075d614307ae
Juny0110 <wnsdyds403@naver.com>
2022-08-15 16:39:59 +0900

    Feat:sparkels:: #82 Add CourseListView
    - 역사탐방 리스트 레이아웃 추가
    - JSON 및 이미지 불러오기

---
+++ IntoHistory/CourseListView/CourseListCell.swift
@@ -0,0 +1,83 @@
+//
+//  CourseListCell.swift
+//  IntoHistory
+//
+//  Created by 지준용 on 2022/08/13.
+//
+
+import UIKit
+
+class CourseListCell: UICollectionViewCell {
+
+    // MARK: - Property
+
+    static let identifier = "courseListCell"
+    static let courseJSONLoader = LoadingCourseJSON().courses
+
+    let uiComponent: CourseListUIComponents = {
+        return $0
+    }(CourseListUIComponents())
+
+    // MARK: - View
+
+    let resistanceCourseImage: UIImageView = {
+        $0.image = UIImage(named: ImageLiteral.resistanceCourse)
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.contentMode = .scaleAspectFit
+        return $0
+    }(UIImageView())
+    
+    let hStackViewOutside: UIStackView = {
+        $0.axis = .horizontal
+        $0.backgroundColor = .white
+        return $0
+    }(UIStackView())
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+        setupCell()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Method
+
+    private func layout() {
+
+        addSubview(hStackViewOutside)
+        hStackViewOutside.addArrangedSubview(resistanceCourseImage)
+        hStackViewOutside.addArrangedSubview(uiComponent)
+        
+        hStackViewOutside.uiViewShadow(backgroundView: hStackViewOutside)
+    }
+
+    func setupCell() {
+        hStackViewOutside.anchor(
+            top: topAnchor,
+            left: leftAnchor,
+            bottom: bottomAnchor,
+            right: rightAnchor
+        )
+
+        resistanceCourseImage.anchor(
+            top: hStackViewOutside.topAnchor,
+            left: hStackViewOutside.leftAnchor,
+            bottom: hStackViewOutside.bottomAnchor,
+            right: uiComponent.leftAnchor,
+            paddingLeft: 20,
+            paddingBottom: 20,
+            paddingRight: 20,
+            width: 90,
+            height: 90
+        )
+
+        resistanceCourseImage.centerY(inView: hStackViewOutside)
+
+        uiComponent.centerY(inView: hStackViewOutside)
+    }
+}
