4b75bcc75d63f573b88f05dcee86dedb99751044
JI SOO <dlwltn815@gmail.com>
2022-08-16 21:19:27 +0900

    Feat✨: #54 Modify monitoring location
    - wip
    
    Co-Authored-By: GODNOEL <103012104+GODNOEL@users.noreply.github.com>

--- IntoHistory/LocationService.swift
+++ IntoHistory/LocationService.swift
@@ -10,29 +10,73 @@ import CoreLocation
 import UserNotifications
 import UIKit
 
-class LocationService: NSObject {
+class LocationService: NSObject, CLLocationManagerDelegate {
     
     static let shared = LocationService()
     var locationManager: CLLocationManager!
     let courseData = LoadingCourseJSON().courses.map{$0.course_pins}
     var arr = [(Double, Double)]()
+    var allRegions = [CLRegion]()
+
+    var currentLocation : CLLocation?{
+        didSet{
+            evaluateClosestRegions()
+        }
+    }
     
     private override init() {
         super.init()
         locationManager = CLLocationManager.init()
         locationManager.delegate = self
     }
-    
-    func registLocation(lat: Double , long: Double) {
-        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
-        let region = CLCircularRegion(center: location, radius: 100.0, identifier: "id\(location)")
-        region.notifyOnExit = true
-        region.notifyOnExit = true
+
+    func evaluateClosestRegions() {
+        print("evaluateClosest 함수 실행")
+
+        var allDistance : [Double] = []
+
+        //Calulate distance of each region's center to currentLocation
+        for region in allRegions{
+            let circularRegion = region as! CLCircularRegion
+            let distance = currentLocation!.distance(from: CLLocation(latitude: circularRegion.center.latitude, longitude: circularRegion.center.longitude))
+            allDistance.append(distance)
+        }
+        // a Array of Tuples
+        let distanceOfEachRegionToCurrentLocation = zip(allRegions, allDistance)
+
+        //sort and get 20 closest
+        let twentyNearbyRegions = distanceOfEachRegionToCurrentLocation
+            .sorted{ tuple1, tuple2 in return tuple1.1 < tuple2.1 }
+            .prefix(20)
+
+        // Remove all regions you were tracking before
+        for region in locationManager.monitoredRegions{
+            locationManager.stopMonitoring(for: region)
+        }
+
+        twentyNearbyRegions.forEach{
+            ($0.0).notifyOnEntry = true
+            ($0.0).notifyOnExit = true
+            locationManager.startMonitoring(for: $0.0)
+        }
+    }
+
+    func registLocation() {
+
+//        // 60개 이상 전부 CLRegion으로 만들어
+//        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+//        let region = CLCircularRegion(center: location, radius: 100.0, identifier: "id\(location)")
+//
+//        //사용자 위치 기반으로 20개 계싼해서 업데이트
+
+//        region.notifyOnEntry = true
+//        region.notifyOnExit = true
 
         locationManager.allowsBackgroundLocationUpdates = true
         locationManager.pausesLocationUpdatesAutomatically = false
         locationManager.startUpdatingLocation()
-        locationManager.startMonitoring(for: region)
+        // 20개만 모니터링중
+//        locationManager.startMonitoring(for: region)
     }
     
     func fireNotification(_ title: String = "Background Test", body: String){
@@ -80,12 +124,16 @@ class LocationService: NSObject {
         for i in 0..<arr.count {
             let lat = arr[i].0
             let long = arr[i].1
-            registLocation(lat: lat, long: long)
+
+            // allRegions 생성
+            let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            let region = CLCircularRegion(center: location, radius: 100.0, identifier: "id\(location)")
+            allRegions.append(region)
         }
     }
 }
 
-extension LocationService: CLLocationManagerDelegate {
+extension LocationService {
 
     func requestAlwaysLocation() {
         switch locationManager.authorizationStatus {
@@ -100,7 +148,9 @@ extension LocationService: CLLocationManagerDelegate {
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {}
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        print("didStartMonitoringFor\(region)")
+    }
     
     func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
         switch state {
