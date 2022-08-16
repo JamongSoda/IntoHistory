c1e8b4a5b146fe5afd1294621ead943e2c948ff4
GODNOEL <ah2650@naver.com>
2022-08-16 23:45:19 +0900

    Feat✨: #54 fix Notification Code
    지역을 20개 밖에 모니터링 못해서...
    동적으로 사용자 근처의 20개의 지역을 모니터링할 수 있도록 코드 수정함;

--- IntoHistory/LocationService.swift
+++ IntoHistory/LocationService.swift
@@ -16,45 +16,47 @@ class LocationService: NSObject {
     var locationManager: CLLocationManager!
     let courseData = LoadingCourseJSON().courses.map{$0.course_pins}
     var arr = [(Double, Double)]()
-
+    var allRegions = [CLRegion]()
+    var currentLocation : CLLocation?{
+        didSet{
+            evaluateClosestRegions()
+        }
+    }
+    
     private override init() {
         super.init()
         locationManager = CLLocationManager.init()
         locationManager.delegate = self
     }
-
+    
     func registLocation(lat: Double , long: Double) {
-        let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
-        let region = CLCircularRegion(center: location, radius: 100.0, identifier: "id\(location)")
-        region.notifyOnExit = true
-        region.notifyOnExit = true
-
         locationManager.allowsBackgroundLocationUpdates = true
         locationManager.pausesLocationUpdatesAutomatically = false
         locationManager.startUpdatingLocation()
-        locationManager.startMonitoring(for: region)
     }
+    
+    func evaluateClosestRegions() {
+        var allDistance : [Double] = []
 
-    func fireNotification(_ title: String = "Background Test", body: String){
-        let notificationCenter = UNUserNotificationCenter.current()
+           for region in allRegions{
+               let circularRegion = region as! CLCircularRegion
+               let distance = currentLocation!.distance(from: CLLocation(latitude: circularRegion.center.latitude, longitude: circularRegion.center.longitude))
+               allDistance.append(distance)
+           }
 
-        notificationCenter.getNotificationSettings{
-            if $0.alertSetting == .enabled {
-                let content = UNMutableNotificationContent()
-                content.title = title
-                content.body = body
+           let distanceOfEachRegionToCurrentLocation = zip(allRegions, allDistance)
 
-                let uuidString = UUID().uuidString
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
-                let request = UNNotificationRequest(identifier: "test-\(uuidString)", content: content, trigger: trigger)
-                notificationCenter.add(request, withCompletionHandler: { (error) in
-                    if error != nil {
-                        // error
-                    }
-                })
+           let twentyNearbyRegions = distanceOfEachRegionToCurrentLocation
+               .sorted{ tuple1, tuple2 in return tuple1.1 < tuple2.1 }
+               .prefix(20)
 
-            }
-        }
+           for region in locationManager.monitoredRegions{
+               locationManager.stopMonitoring(for: region)
+           }
+
+           twentyNearbyRegions.forEach{
+               locationManager.startMonitoring(for: $0.0)
+           }
     }
 
     private func loadJSONData() {
@@ -81,9 +83,37 @@ class LocationService: NSObject {
             let lat = arr[i].0
             let long = arr[i].1
             registLocation(lat: lat, long: long)
+
+            let location = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            let region = CLCircularRegion(center: location, radius: 100.0, identifier: "id\(location)")
+            region.notifyOnEntry = true
+            allRegions.append(region)
         }
     }
 }
+
+    func fireNotification(_ title: String = "Background Test", body: String){
+        let notificationCenter = UNUserNotificationCenter.current()
+
+        notificationCenter.getNotificationSettings{
+            if $0.alertSetting == .enabled {
+                let content = UNMutableNotificationContent()
+                content.title = title
+                content.body = body
+
+                let uuidString = UUID().uuidString
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
+                let request = UNNotificationRequest(identifier: "test-\(uuidString)", content: content, trigger: trigger)
+                notificationCenter.add(request, withCompletionHandler: { (error) in
+                    if error != nil {
+                        // error
+                    }
+                })
+            }
+        }
+    }
+
+   
 
 extension LocationService: CLLocationManagerDelegate {
 
@@ -100,7 +130,7 @@ extension LocationService: CLLocationManagerDelegate {
         }
     }
 
-    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {}
+//    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {}
 
     func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
         switch state {
@@ -113,15 +143,12 @@ extension LocationService: CLLocationManagerDelegate {
         // 해당 지역에 들어오고 나가면 프린트로 찍힘
     }
 
-//    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-//        print("들어옴 ]\(region)")
-//        fireNotification("영웅의 길 정.복.완.료😎",body: "방문완료!\(region)")
-//    }
-
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
-            print("위도: \(location.coordinate.latitude)")
-            print("경도: \(location.coordinate.longitude)")
+        if let nowLocation = locations.first {
+            print("위도: \(nowLocation.coordinate.latitude)")
+            print("경도: \(nowLocation.coordinate.longitude)")
+            
+            guard let location = locations.last else { return};currentLocation = location
         }
     }
 
