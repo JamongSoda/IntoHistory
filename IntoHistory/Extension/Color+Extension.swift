942d2539387cfc7e2869f61ff73d44a211272dc5
JI SOO <dlwltn815@gmail.com>
2022-08-17 13:07:37 +0900

    Refactor♻️: #117 Change parameter name
    - uiview에서 사용하는 color extension이름 변경

--- IntoHistory/Extension/Color+Extension.swift
+++ IntoHistory/Extension/Color+Extension.swift
@@ -29,9 +29,9 @@ extension UIColor {
 }
 
 extension UIView {
-    func setGradient(color1:UIColor,color2:UIColor){
+    func setGradient(startColor :UIColor, endColor: UIColor){
         let gradient: CAGradientLayer = CAGradientLayer()
-        gradient.colors = [color1.cgColor,color2.cgColor]
+        gradient.colors = [startColor.cgColor, endColor.cgColor]
         gradient.locations = [0.0 , 1.0]
         gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
         gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
