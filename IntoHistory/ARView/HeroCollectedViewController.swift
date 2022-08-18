a4d97e4b33d5a41f8ccd84087f624313fcce7edc
JI SOO <dlwltn815@gmail.com>
2022-08-18 13:39:28 +0900

    Feat✨: #136 Add new Collected view
    - 맨 마지막에 영웅 수집하고 띄울 view 생성

---
+++ IntoHistory/ARView/HeroCollectedViewController.swift
@@ -0,0 +1,11 @@
+//
+//  HeroCollectedViewController.swift
+//  IntoHistory
+//
+//  Created by LeeJiSoo on 2022/08/18.
+//
+
+import UIKit
+
+class HeroCollectedViewController: UIViewController {
+}
