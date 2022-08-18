8855ec24ef9b5e41f67106f3d9134a89cdaf9654
taek0622 <min4644@gmail.com>
2022-08-18 15:47:06 +0900

    !HOTFIX:ambulance:: #140 Fix NMap Memory Leak
    - NMapViewController에 진입했다가 나왔을 시 메모리가 해제되지 않고 지속되어 메모리 누수가 일어나서 앱이 뻗어버리는 현상 해결

--- IntoHistory/NMapViewController.swift
+++ IntoHistory/NMapViewController.swift
@@ -8,12 +8,16 @@ import CoreLocation
 import UIKit
 
 import NMapsMap
-	
+
+let naverMapView = NMFNaverMapView()
+
 class NMapViewController: UIViewController, CLLocationManagerDelegate {
 
     // MARK: - Property
 
     var locationManager = CLLocationManager()
+    var booleanArray = [Bool]()
+    var markers = [NMFMarker()]
 
     // MARK: - View
 
@@ -53,7 +57,8 @@ class NMapViewController: UIViewController, CLLocationManagerDelegate {
 
         attribute()
 
-        let naverMapView = NMFNaverMapView(frame: view.frame)
+        naverMapView.frame = view.frame
+        naverMapView.mapView.addCameraDelegate(delegate: self)
         naverMapView.showLocationButton = true
         naverMapView.showZoomControls = false
 
@@ -124,9 +129,6 @@ class NMapViewController: UIViewController, CLLocationManagerDelegate {
 
         titleLabel.text = coreDataManager.coursePins[0].pinName
         addressLabel.text = coreDataManager.coursePins[0].address
-
-        var booleanArray = [Bool]()
-        var markers = [NMFMarker()]
 
         for pinNum in 0..<coreDataManager.coursePins.count {
 
