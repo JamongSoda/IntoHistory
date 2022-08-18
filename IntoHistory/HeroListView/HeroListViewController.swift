7c1b388f878aebcc5649735217c12fe3e3e74897
taek0622 <min4644@gmail.com>
2022-08-18 20:47:15 +0900

    Fix:beetle:: Fix all hero is already collected
    - 아직 해금하지 않은 영웅들도 모두 잠금 해제되어있는 버그 수정

--- IntoHistory/HeroListView/HeroListViewController.swift
+++ IntoHistory/HeroListView/HeroListViewController.swift
@@ -81,14 +81,12 @@ extension HeroListViewController: UICollectionViewDataSource, UICollectionViewDe
             return cell
         } else if indexPath.section == 1 {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HeroListCell.identifier, for: indexPath) as! HeroListCell
-            coreDataManager.resistances[indexPath.row].isCollected = true
             cell.imageView.image = UIImage(named: coreDataManager.resistances[indexPath.row].isCollected ? coreDataManager.resistances[indexPath.row].image : ImageLiteral.lockedHero)
             cell.labelView.text = coreDataManager.resistances[indexPath.row].isCollected ? coreDataManager.resistances[indexPath.row].heroName : "대한민국의 영웅"
             
             return cell
         } else {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HeroListCell.identifier, for: indexPath) as! HeroListCell
-            coreDataManager.warriors[indexPath.row].isCollected = true
             cell.imageView.image = UIImage(named: coreDataManager.warriors[indexPath.row].isCollected ? coreDataManager.warriors[indexPath.row].image : ImageLiteral.lockedHero)
             cell.labelView.text = coreDataManager.warriors[indexPath.row].isCollected ? coreDataManager.warriors[indexPath.row].heroName : "대한민국의 영웅"
             
